ad154ecb09f8b9e03cc9dbb3739fbfc3c608ea2d
Julian Gommans <julian.s.gommans@gmail.com>
2017-05-31 13:12:02 +1200

    react.js locally is the suck

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -1,50 +1,63 @@
-<!DOCTYPE=html>
-<html>
-	<head>
-		<link rel="stylesheet" href="../libs/tictactoe.css" />
-		<script type="text/javascript" src='../libs/tictactoe.js'></script>
-		<script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/react/0.14.7/react-with-addons.js'></script>
-		<script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/react/0.14.7/react-dom.js'></script>
-	</head>
-	<body>
-		<div id="errors" style="
-		  background: #c00;
-		  color: #fff;
-		  display: none;
-		  margin: -20px -20px 20px;
-		  padding: 20px;
-		  white-space: pre-wrap;
-		"></div>
-		<div id="root"></div>
-		<script>
-		window.addEventListener('mousedown', function(e) {
-		  document.body.classList.add('mouse-navigation');
-		  document.body.classList.remove('kbd-navigation');
-		});
-		window.addEventListener('keydown', function(e) {
-		  if (e.keyCode === 9) {
-		    document.body.classList.add('kbd-navigation');
-		    document.body.classList.remove('mouse-navigation');
-		  }
-		});
-		window.addEventListener('click', function(e) {
-		  if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
-		    e.preventDefault();
-		  }
-		});
-		window.onerror = function(message, source, line, col, error) {
-		  var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
-		  errors.textContent += text + '\n';
-		  errors.style.display = '';
-		};
-		console.error = (function(old) {
-		  return function error() {
-		    errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
-		    errors.style.display = '';
-		    old.apply(this, arguments);
-		  }
-		})(console.error);
-		</script>
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
 
-	</body>
-</html>
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
